0ce12ef85437dc030a6f30b45f2d7c643fe7ac47
Kim <kime924.dev@live.com.sg>
2015-03-13 16:53:45 +0800

    Fixed aiguitar-node.js

--- airguitar-node.js
+++ airguitar-node.js
@@ -3,3 +3,9 @@ var app = express();
 
 //tells the server to look into the /public folder for the static content
 app.use(express.static(__dirname + '/AirGuitar/'));
+
+var server = app.listen(process.env.PORT || 3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('app listening at %s:%s', host, port);
+})
